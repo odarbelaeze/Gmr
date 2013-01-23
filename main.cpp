e32694a6be048b4fbf0bba839f9fd8aaca78c4a3
Juan <jdalzatec@unal.edu.co>
2013-01-23 12:39:49 -0500

    try to do hamiltonians

--- main.cpp
+++ main.cpp
@@ -17,6 +17,27 @@ float electric (Particle & p, Field & e)
         inner_prod (e.direction, p.state.r);
 }
 
+float el_primero (Particle & p1, Particle & p2){
+    return - inner_prod (p1.state.s, p2.state.s);
+}
+
+
+float H_r (Particle & p1, vector<Particle> S){
+    float sum = 0;
+    for (int i = 0; i < S.size(); ++i)
+    {
+        sum += exp (norm_2 (p1.state.r - S[i].state.s) * inner_prod(p1.state.s, S[i].state.s);
+    }
+
+    return - sum;
+
+
+}
+
+
+
+
+
 int main(int argc, char const *argv[])
 {
     boost::numeric::ublas::vector<float> r;
