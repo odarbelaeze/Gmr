622db6d1b2a7ce59f69193008fe5e4ac69fb2c26
Oscar David Arbeláez <odarbelaeze@gmail.com>
2013-02-06 18:55:51 -0500

    Fixing my student.

--- test.cpp
+++ test.cpp
@@ -4,7 +4,6 @@
 
 using namespace boost::numeric::ublas;
 
-<<<<<<< HEAD
 float distancia(Particle a, Particle b)
 {
     float dist;
@@ -12,8 +11,6 @@ float distancia(Particle a, Particle b)
     return dist;
 }
 
-=======
->>>>>>> e29359eab86452b9a1b77c2d7079086d90975496
 int main(int argc, char const *argv[])
 {
     std::vector<Particle> P;
@@ -35,27 +32,8 @@ int main(int argc, char const *argv[])
     System A(root);
     P = A.getParticles();
 
-    // TR(i, P)
-    // {
-    //     std::cout << (*i).state.r(0) << "\t" << (*i).state.r(1) << "\t" << (*i).state.r(2) << std::endl;
-    // }
-
-    // TR(i, P)
-    // {
-    //     std::cout << (*i).traits.name << "\t" << (*i).traits.charge << "\t" << (*i).traits.s_norm << "\t" << (*i).state.r(0) << "\t" << (*i).state.r(1) << "\t" << (*i).state.r(2) << std::endl;
-    // }
-
     float banderita;
     float radio;
-
-    radio = system_info["cut_off"].asFloat();
-    TR(i, P)
-    {
-        TR(j, (*i).nbh)
-        {
-            banderita = distancia(*i, **j);
-        }
-    }
 
 
     return 0;
